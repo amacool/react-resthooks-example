0a3b971db66740ffd4441a1c2ec1880407180a74
Cool Dev <amacool0117@outlook.com>
2019-12-22 00:12:00 +0100

    Updates

---
+++ precache-manifest.bbd7ea59d22c7e697ca745b4fb1211b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa748e282aa6d9696f7c67eccb222ea8",
+    "url": "/react-resthooks-example/index.html"
+  },
+  {
+    "revision": "3e4ab7ccf4a436f302fd",
+    "url": "/react-resthooks-example/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5692849238f43a2241c9",
+    "url": "/react-resthooks-example/static/js/2.9b476d28.chunk.js"
+  },
+  {
+    "revision": "3e4ab7ccf4a436f302fd",
+    "url": "/react-resthooks-example/static/js/main.a4843c68.chunk.js"
+  },
+  {
+    "revision": "232b3473b885703af557",
+    "url": "/react-resthooks-example/static/js/runtime-main.7f4c32a7.js"
+  }
+]);
